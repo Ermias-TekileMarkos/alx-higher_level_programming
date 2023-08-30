4c62f96c7ef426019dd1e4885725622f2bf866c8
Ermias-TekileMarkos <ermaatekk@gmail.com>
2023-08-30 19:27:14 +0000

    0x06-python-classes

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Create Node class."""
+
+
+class Node:
+    """Defining Node linked list."""
+
+    def __init__(self, data, next_node=None):
+        """Initialize a new Node.
+
+        Args:
+            data (int): The data of the new Node.
+            next_node (Node): The next node of the new Node.
+        """
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        """Get/set the data of the Node."""
+        return (self.__data)
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+all abt class and object
